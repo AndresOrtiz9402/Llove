516317ef7f1e8fe22d90368c74d2a6097f693bda
andres <andresortiz9402@gmail.com>
2025-01-11 11:45:07 -0400

    Feature(apps): An endpoint was added to bff-api to connect with OpenAI through letter-api.

--- apps/bff-api/src/app/app.controller.ts
+++ apps/bff-api/src/app/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { Bff } from '@llove/product-domain/backend';
+
+type PostLetterDto = Bff.Infrastructure.Dtos.PostLetterDto;
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('post-letter')
+  postLetter(@Body() req: PostLetterDto) {
+    const { message } = req;
+    return this.appService.postLetter(message);
   }
 }
--- apps/bff-api/src/app/app.service.ts
+++ apps/bff-api/src/app/app.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
 
+import { Bff } from '@llove/product-domain/backend';
 @Injectable()
-export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
-  }
-}
+export class AppService extends Bff.Application.BffService {}
