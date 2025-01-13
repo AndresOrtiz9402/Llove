00c47988bafc34e05ded315f4df1a51444510239
andres <andresortiz9402@gmail.com>
2025-01-13 14:21:27 -0400

    Fix(apps): The new configuration methods were added to the letter API services.

--- apps/letter-api/src/app/app.service.ts
+++ apps/letter-api/src/app/app.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 import { Letter } from '@llove/product-domain/backend';
 
+import { Config } from '..';
+
 @Injectable()
-export class AppService extends Letter.Application.LetterService {}
+export class AppService extends Letter.Application.LetterService {
+  constructor() {
+    super(new OpenAI({ apiKey: Config.OPENAI_API_KEY }));
+  }
+}
