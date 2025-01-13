dc4ba1d4135296d764feba2e23cf2e64d847455a
andres <andresortiz9402@gmail.com>
2025-01-13 14:06:09 -0400

    Feature(libs): The Bff services were updated through their library in product-domain/backend.

--- libs/product-domain/backend/src/bff/application/bff.service.ts
+++ libs/product-domain/backend/src/bff/application/bff.service.ts
@@ -1,11 +1,22 @@
+import { type Infrastructure as LetterInfrastructure } from '../../letter';
 import { UseCase } from '.';
 
 export class BffService {
-  constructor(
-    private readonly LETTER_API_URL = new URL(process.env.LETTER_API_URL)
-  ) {}
+  constructor(private readonly BFF_ENV: Record<string, URL>) {}
 
-  postLetter(message: string) {
-    return UseCase.postLetter({ message }, this.LETTER_API_URL);
+  postLetterCompletions(message: string) {
+    return UseCase.httpSimplePostRequest(
+      { message },
+      this.BFF_ENV.LETTER_API_COMPLETIONS_URL as URL
+    );
+  }
+
+  letterGeneratePetition(LetterDto: LetterInfrastructure.Dtos.LetterDto) {
+    //TODO: User validation.
+
+    return UseCase.httpSimplePostRequest(
+      LetterDto,
+      this.BFF_ENV.LETTER_API_LETTER_GENERATE_URL
+    );
   }
 }
