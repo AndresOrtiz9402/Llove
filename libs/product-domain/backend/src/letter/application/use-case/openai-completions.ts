dc34ea0a0da11f6c235606b8d947112b0ffbd1ca
andres <andresortiz9402@gmail.com>
2025-01-13 11:56:39 -0400

    Feature(libs): A new method was added to the use cases for letter generation in the product-domain letter library.

--- libs/product-domain/backend/src/letter/application/use-case/openai-completions.ts
+++ libs/product-domain/backend/src/letter/application/use-case/openai-completions.ts
@@ -1,10 +1,16 @@
 import { Openai } from '@llove/backend';
 
-type OpenaiMessage = Openai.OpenaiUseCase;
+import { type Infrastructure } from '../..';
+
+type OpenaiMessage<T> = Openai.OpenaiUseCase<T>;
 type ChatCompletionCreateParamsNonStreaming =
   Openai.ChatCompletionCreateParamsNonStreaming;
+type LetterDto = Infrastructure.Dtos.LetterDto;
 
-export const openaiMessage: OpenaiMessage = async (content, openai) => {
+export const openaiSimpleCompletion: OpenaiMessage<string> = async (
+  content,
+  openai
+) => {
   const noStreamParams: ChatCompletionCreateParamsNonStreaming = {
     model: 'gpt-4o-mini',
     messages: [
@@ -17,9 +23,55 @@ export const openaiMessage: OpenaiMessage = async (content, openai) => {
 
   const completions = await Openai.noStreamCompletions(openai, noStreamParams);
 
-  const completionsJson = JSON.stringify({
+  const res = JSON.stringify({
     message: completions.choices[0].message.content,
   });
 
-  return completionsJson;
+  return res;
+};
+
+export const openaiLetterGenerate: OpenaiMessage<LetterDto> = async (
+  content,
+  openai
+) => {
+  const prompt = `
+    Vas a recibir un JSON con el siguiente formato:
+
+    {
+        "isFor": "string" ,
+        "occasion": "string", 
+        "relationship": "string", 
+        "tone": "string",
+        "language": "spanish",
+    }
+
+    Vas a usar este JSON para generar el contenido para una carta.
+
+    La carta debe tener un membrete, un cuerpo y una despedida.
+  `;
+  //TODO: add emojis option.
+  //TODO: add keywords option.
+
+  const jsonContent = JSON.stringify({ ...content, language: 'spanish' });
+
+  const noStreamParams: ChatCompletionCreateParamsNonStreaming = {
+    model: 'gpt-4o-mini',
+    messages: [
+      { role: 'system', content: prompt },
+      {
+        role: 'user',
+        content: jsonContent,
+      },
+    ],
+  };
+
+  const completions = await Openai.noStreamCompletions(openai, noStreamParams);
+
+  const letterContent = completions.choices[0].message.content;
+
+  const res = JSON.stringify({
+    letter: letterContent,
+  });
+
+  return res;
 };
