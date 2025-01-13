7ee8716777c5a6f7ac824a4a3fc5fd55d08bada1
andres <andresortiz9402@gmail.com>
2025-01-13 12:32:52 -0400

    Fix(libs): The example methods were removed from the User library in product-domain/backend.

--- libs/product-domain/backend/src/user/application/index.ts
+++ libs/product-domain/backend/src/user/application/index.ts
@@ -1,3 +1,2 @@
 export * from './user.service';
-export * from './find-by-id.use-case';
 export * from './user-not-found.exception';
--- libs/product-domain/backend/src/user/application/user.service.ts
+++ libs/product-domain/backend/src/user/application/user.service.ts
@@ -1,12 +1,3 @@
 import { User } from '@llove/models';
-import { UserFindByIdUseCase } from '.';
 
-export class UserService {
-  constructor(private readonly userFindByIdUseCase: UserFindByIdUseCase) {}
-
-  findById(
-    params: User.GetByIdParam
-  ): Promise<User.infrastructure.UserResponse> {
-    return this.userFindByIdUseCase.run(params);
-  }
-}
+export class UserService {}
