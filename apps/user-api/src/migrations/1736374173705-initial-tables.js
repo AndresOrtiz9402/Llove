151096b19c97c4aeb4da2828ff2179a9b7f0c382
andres <andresortiz9402@gmail.com>
2025-01-15 14:49:23 -0400

    Fix(user-api): User-api migration was fixed.

--- apps/user-api/src/migrations/1736374173705-initial-tables.js
+++ apps/user-api/src/migrations/1736374173705-initial-tables.js
@@ -1,14 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { MigrationInterface, QueryRunner } = require('typeorm');
 
 module.exports = class InitialTables1736374173705 {
   async up(queryRunner) {
     await queryRunner.query(`
       CREATE TABLE "users" (
-        "id" int PRIMARY KEY,
+        "id" SERIAL PRIMARY KEY,
         "name" varchar(50) UNIQUE NOT NULL,
         "email" varchar(100) UNIQUE NOT NULL,
-        "created_at" timestamp NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
-        "deleted_at" timestamp DEFAULT 'NULL'
+        "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+        "deleted_at" timestamp DEFAULT (NULL)
       )`);
   }
 
