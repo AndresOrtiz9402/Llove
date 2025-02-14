b36799bd47df47cce1630660fa4eaf446c66059e
andres <andresortiz9402@gmail.com>
2025-02-14 16:12:28 -0400

    Feature(models): The new structure for SQL queries has been added to the base repository model.

---
+++ libs/models/src/shared/data-access/base.repository.ts
@@ -0,0 +1,28 @@
+import type { OmitBaseEntity, Id } from '..';
+import { Interfaces } from '..';
+import { Query } from '.';
+
+type BaseEntity<Entity, Id> = Entity & { id: Id };
+
+type Input<Entity, OmitBase> = {
+  [P in Exclude<keyof Entity, OmitBase>]: Entity[P];
+};
+
+type SuccessOrError<R> = Interfaces.SuccessOrError.SuccessOrError<unknown, R>;
+
+export type BaseRepository<Entity> = {
+  create(input: Input<Entity, OmitBaseEntity>): Promise<SuccessOrError<BaseEntity<Entity, Id>>>;
+
+  deletedById(input: Id): Promise<SuccessOrError<unknown>>;
+
+  getAll(): Promise<SuccessOrError<BaseEntity<Entity, Id>[]>>;
+
+  getById(input: Id): Promise<SuccessOrError<BaseEntity<Entity, Id>>>;
+
+  updateById(
+    id: Id,
+    updateInput: Partial<Input<Entity, OmitBaseEntity>>
+  ): Promise<SuccessOrError<unknown>>;
+
+  getPage(queryObj: Query.QueryObj<Entity>): Promise<SuccessOrError<BaseEntity<Entity, Id>[]>>;
+};
