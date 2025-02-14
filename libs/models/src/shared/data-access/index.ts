98bce2cd2cea7f59909f8495532a3f5c222b31ff
andres <andresortiz9402@gmail.com>
2025-02-14 16:13:45 -0400

    Fix(models): All the data access logic has been moved to the DataAccess package.

---
+++ libs/models/src/shared/data-access/index.ts
@@ -0,0 +1,4 @@
+export * from './base.repository';
+
+export * as Query from './base.query';
+export * as Transactions from './base.transaction';
--- libs/models/src/shared/index.ts
+++ libs/models/src/shared/index.ts
@@ -1,8 +1,7 @@
 export * from './base.ai-service';
 export * from './base.entity';
 export * from './base.input';
-export * from './base.repository';
 
 export * as DbConnection from './db-connection.interface';
 export * as Interfaces from './interfaces';
-export * as Transactions from './base.transaction';
+export * as DataAccess from './data-access';
