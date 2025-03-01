090314fe44d87bd51fbe7f30c132619eb8543c54
andres <andresortiz9402@gmail.com>
2025-03-01 07:39:53 -0400

    Feature(bff-api): "Cookie handling was enabled in the bff-api.

--- apps/bff-api/src/main.ts
+++ apps/bff-api/src/main.ts
@@ -1,9 +1,15 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+
 import { BffModule } from './app/bff.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BffModule);
+
+  /* app.enableCors(); */
+
+  app.use(cookieParser());
 
   const globalPrefix = 'bff-api';
   app.setGlobalPrefix(globalPrefix);
@@ -19,9 +25,9 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  Logger.log(
-    `🚀 [Bff-api]: Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 [Bff-api]: Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
+
+//TODO: add https.
