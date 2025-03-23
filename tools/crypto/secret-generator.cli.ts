5447325865bb6a5299584873e194fbc189716a26
andres <andresortiz9402@gmail.com>
2025-03-23 17:30:23 -0400

    update(root): update project dependencies and remove unused tools library.

--- tools/crypto/secret-generator.cli.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'crypto';
-
-const secret: string = randomBytes(32).toString('hex');
-
-console.log('Secreto:', secret);
--- tools/index.ts
+++
@@ -1,1 +0,0 @@
-export * as Testing from './testing';
